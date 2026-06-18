594490d123f4886ddf6f995b2a3873ec34b3b440
Nicholas William <nicholaswilliam2307@gmail.com>
2026-06-18 22:48:15 +1000

    feat(03-02): create lib/data/contact.ts with social link data

---
+++ lib/data/contact.ts
@@ -0,0 +1,10 @@
+export const socialLinks = {
+  github: {
+    url: 'https://github.com/nichowil',
+    label: 'Nicholas on GitHub',
+  },
+  linkedin: {
+    url: 'https://linkedin.com/in/nichowil',
+    label: 'Nicholas on LinkedIn',
+  },
+};
